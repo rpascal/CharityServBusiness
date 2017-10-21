a39f24b400926e55c769440b63558c5607df80a1
rpascal <ryanpascal99@yahoo.com>
2017-10-21 15:53:13 -0400

    adding addition field to sign up form

---
+++ src/models/googleLocationDetails.ts
@@ -0,0 +1,14 @@
+export interface googleLocationsDetails{
+     formatted_address : string;
+    // formatted_phone_number : string;
+    // lat :number;
+    // lng : number;
+    // international_phone_number : string;
+    // name : string;
+    // website : string;
+    // url : string;
+    Street: string;
+    State: string;
+    City: string;
+    Zip: string;
+}
